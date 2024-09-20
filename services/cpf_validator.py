eb73d34ecc1b04cc7043fc0046dbf50e5c24d171
gilsonmneto <gilsonmneto@yahoo.com.br>
2024-09-20 02:20:53 -0300

    tratamento erro

--- services/cpf_validator.py
+++ services/cpf_validator.py
@@ -10,36 +10,39 @@ class CPFValidator:
     def is_cpf_valid(self, cpf):
         """Valida o CPF"""
 
-        data = self._is_valid_input(cpf)
-        if data:
+        try:
+            data = self._is_valid_input(cpf)
+            if data:
+                return self.response_factory.create_response(
+                    "OK", 200, data, "CPF string inválida"
+                )
+
+            cpf = self._sanitize_cpf(cpf)
+
+            data = self._is_length_valid(cpf)
+            if data:
+                return self.response_factory.create_response(
+                    "OK", 200, data, "CPF não tem 11 dígitos"
+                )
+
+            data = self._is_first_digit_valid(cpf)
+            if data:
+                return self.response_factory.create_response(
+                    "OK", 200, data, "Dígito verificador inválido"
+                )
+
+            data = self._is_second_digit_valid(cpf)
+            if data:
+                return self.response_factory.create_response(
+                    "OK", 200, data, "Dígito verificador inválido"
+                )
+
+            data = {"cpf": cpf, "is_valid": True}
             return self.response_factory.create_response(
-                "OK", 200, data, "CPF string inválida"
+                "OK", 200, data, "CPF informado é válido"
             )
-
-        cpf = self._sanitize_cpf(cpf)
-
-        data = self._is_length_valid(cpf)
-        if data:
-            return self.response_factory.create_response(
-                "OK", 200, data, "CPF não tem 11 dígitos"
-            )
-
-        data = self._is_first_digit_valid(cpf)
-        if data:
-            return self.response_factory.create_response(
-                "OK", 200, data, "Dígito verificador inválido"
-            )
-
-        data = self._is_second_digit_valid(cpf)
-        if data:
-            return self.response_factory.create_response(
-                "OK", 200, data, "Dígito verificador inválido"
-            )
-
-        data = {"cpf": cpf, "is_valid": True}
-        return self.response_factory.create_response(
-            "OK", 200, data, "CPF informado é válido"
-        )
+        except Exception as e:
+            return self.response_factory.create_response("ERROR", 500, None, str(e))
 
     def _is_valid_input(self, cpf):
         if not cpf or not isinstance(cpf, str):
